ed572de67d0a91dcfafb6cb4bc3459fecb819c51
ch7025 <cjh7025@uncw.edu>
2024-10-12 21:05:07 -0400

    created Ship and Boat classes

--- src/Boat.java
+++ src/Boat.java
@@ -1,2 +1,50 @@
-public class Boat {
+//Worked on by Connor Harmon 10/12/2024, created boat class with constructor, setters/getters, and toString
+
+/**
+ * The boat class is a subclass of WaterTransportation
+ * that represents a boat object
+ * contains an operatorType variable that says whether the
+ * boat is owned by an individual or a company
+ */
+public class Boat extends WaterTransportation{
+    //attributes
+    private String operatorType;
+
+    /**
+     * creates a boat object with all attributes and superclass attributes
+     * @param capacity num of passengers boat can safely handle
+     * @param maxSpeed fastest speed boat can safely travel
+     * @param cost the cost to ride the boat
+     * @param ticketOrRental whether the user is buying a ticket or renting the boat
+     * @param poweredBy how the boat is powered
+     * @param operatorType whether the boat is owned by an individual or company, must input "individual" or "company"
+     *                or variable will be set to an empty string
+     */
+    public Boat(int capacity, float maxSpeed, double cost, String ticketOrRental, String poweredBy, String operatorType){
+        super(capacity, maxSpeed, cost, ticketOrRental, poweredBy);
+
+        //validity check for ownedBy
+        if(operatorType.equalsIgnoreCase("individual")){
+            this.operatorType = "individual";
+        }
+        else if(operatorType.equalsIgnoreCase("company")){
+            this.operatorType = "company";
+        }
+        else{
+            this.operatorType = "";
+        }
+    }
+
+    public String getOperatorType(){
+        return operatorType;
+    }
+
+    public void setOperatorType(String ownedBy){
+        this.operatorType = ownedBy;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ", owned by: " + operatorType;
+    }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,54 @@
-public class Ship {
+//Worked on by Connor Harmon 10/12/2024, created ship class with constructor, setters/getters, and toString
+
+/** The Ship class is a subclass of the WaterTransportation class
+ * that represents a ship object.
+ * Contains an overnight variable to signify if the voyage is overnight
+ */
+public class Ship extends WaterTransportation{
+    //attributes
+    private boolean overnight;
+
+    /**
+     * creates a Ship object with all attributes and superclass attributes
+     * @param capacity num of passengers the boat can safely handle
+     * @param maxSpeed the fastest speed the ship can safely travel
+     * @param cost how much it costs to ride
+     * @param ticketOrRental whether the ship is being rented or ticketed
+     * @param poweredBy how the ship is powered
+     * @param overnight whether the trip lasts overnight
+     */
+    public Ship(int capacity, float maxSpeed, double cost, String ticketOrRental, String poweredBy, boolean overnight){
+        super(capacity, maxSpeed, cost, ticketOrRental, poweredBy);
+        this.overnight = overnight;
+    }
+
+    /**
+     * returns boolean overnight variable
+     * @return overnight
+     */
+    public boolean getOvernight(){
+        return overnight;
+    }
+
+    /**
+     * sets boolean overnight variable
+     * @param overnight whether the trip lasts overnight
+     */
+    public void setOvernight(boolean overnight){
+        this.overnight = overnight;
+    }
+
+    /**
+     * adds ship variables to superclass toString()
+     * @return string
+     */
+    @Override
+    public String toString(){
+        if(overnight) {
+            return super.toString() + ", overnight: " + "yes";
+        }
+        else{
+            return super.toString() + ", overnight: " + "no";
+        }
+    }
 }
