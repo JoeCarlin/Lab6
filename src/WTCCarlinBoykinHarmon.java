7a0ccfb06076ea21f929046d10cb33f4a9b9dc54
ch7025 <cjh7025@uncw.edu>
2024-10-18 09:17:51 -0400

    added watertransportation objects

--- src/WTCCarlinBoykinHarmon.java
+++ src/WTCCarlinBoykinHarmon.java
@@ -3,21 +3,16 @@
 public class WTCCarlinBoykinHarmon {
 
     public static void main(String[] args){
-        String landOptions = land();  // Call land method and return a string of landTransportation objects
-        System.out.println(landOptions);  // Display each objects attributes.
-    }
 
-    /**
-     * Creates objects for each mode of land transportation with appropriate attributes
-     * and converts them to a string for display.
-     *
-     * @return landOptions, The options for land transportation returned as a string.
-     */
-    public static String land(){
+        //land options
         Bus greyhound = new Bus(25,70,8.50,"Ticket", 6, "Yes");
         Train  rockyMountaineer = new Train(100,80,12.75,"Ticket", 30, "Yes");
         Bike schwinn = new Bike(2,30,8.50,"Rental", 2, "Yes");
         Automobile toyota = new Automobile(4,100,32,"Rental", 4, "No");
-        return "Bus: " + greyhound + "\nTrain: " + rockyMountaineer + "\nBike: " +schwinn + "\nAutomobile: " + toyota;
+
+        //water options
+        Submarine Titan = new Submarine(5, 50, 100000, "Ticket", "nuclear", 50000);
+        Boat GenericBoat = new Boat(4, 15, 250, "rental", "sail", "individual");
+        Ship CruiseShip = new Ship(1000, 500, 1000, "ticket", "steam", true);
     }
 }
