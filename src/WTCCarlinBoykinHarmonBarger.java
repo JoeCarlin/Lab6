36cfd5afdfcff0b6afde3061c7c8d191396dc0ee
Jordan Boykin <48491551+jeb6965@users.noreply.github.com>
2024-10-25 09:41:16 -0400

    Comments and javadoc
    Comments and javadoc added to the driver file.
    Prints pickup time for ticket/rentals.

--- src/WTCCarlinBoykinHarmonBarger.java
+++ src/WTCCarlinBoykinHarmonBarger.java
@@ -1,9 +1,25 @@
-//Driver Class
+
 import java.util.Scanner;
 import java.lang.Math;
+
+/**
+ * Main driver class for the transportation lab.
+ * Users get to choose their method of transportation, and it calculates the cost based on the amount of passengers.
+ * @Author Joseph Carlin
+ * @Author Jordan Boykin
+ * @Author Connor Harmon
+ * @Author Emma Barger
+ * @Date: 10/25/24
+ */
 public class WTCCarlinBoykinHarmonBarger {
+    /**
+     * Main method for running the program.
+     * @param args, command line arguments.
+     */
     public static void main(String[] args){
         String confirmation = "yes";
+        // Do while loop for running the program again if user wants to change their selection
+        // or inputs incorrect information.
         do{
             Scanner input1 = new Scanner(System.in);
             // User choice of land, sea, or air transportation options
@@ -43,6 +59,7 @@ public class WTCCarlinBoykinHarmonBarger {
             Transportation transportChoice = null;
             // Switch case for determining which transportation methods to show from user input
             switch (userchoice) {
+                // Land transportation options
                 case "land":
                     System.out.printf("%25s%20s%15s%15s\n", "Cost:", "Ticket or Rental:", "Speed:", "Passengers:");
                     for (int i = 0; i < landNames.length; i++) {
@@ -53,13 +70,14 @@ public class WTCCarlinBoykinHarmonBarger {
                     String landChoice = input2.nextLine();
                     for (int i = 0; i < landNames.length; i++) {
                         if (landChoice.equalsIgnoreCase(landNames[i])) {
+                            // Choice of transport vehicle assigned for future reference
                             transportChoice = landOptions[i];
                             System.out.println(landOptions[i].toString());
                         }
                     }
                     userchoice = landChoice;
                     break;
-
+                // Water transportation options
                 case "sea":
                     System.out.printf("%25s%20s%15s%15s\n", "Cost:", "Ticket or Rental:", "Speed:", "Passengers:");
                     for (int i = 0; i < waterNames.length; i++) {
@@ -70,13 +88,14 @@ public class WTCCarlinBoykinHarmonBarger {
                     String seaChoice = input2.nextLine();
                     for (int i = 0; i < waterNames.length; i++) {
                         if (seaChoice.equalsIgnoreCase(waterNames[i])) {
+                            // Choice of transport vehicle assigned for future reference
                             transportChoice = waterOptions[i];
                             System.out.println(waterOptions[i].toString());
                         }
                     }
                     userchoice = seaChoice;
                     break;
-
+                // Air transportation options
                 case "air":
                     System.out.printf("%25s%20s%15s%15s\n", "Cost:", "Ticket or Rental:", "Speed:", "Passengers:");
                     for (int i = 0; i < airNames.length; i++) {
@@ -87,12 +106,15 @@ public class WTCCarlinBoykinHarmonBarger {
                     String airChoice = input2.nextLine();
                     for (int i = 0; i < airNames.length; i++) {
                         if (airChoice.equalsIgnoreCase(airNames[i])) {
+                            // Choice of transport vehicle assigned for future reference
                             transportChoice = airOptions[i];
                             System.out.println(airOptions[i].toString());
                         }
                     }
                     userchoice = airChoice;
                     break;
+                // If input for transport type is not land, sea, or air
+                // Rerun program
                 default:
                     confirmation = "no";
                     continue;
@@ -106,18 +128,33 @@ public class WTCCarlinBoykinHarmonBarger {
                 Scanner input4 = new Scanner(System.in);
                 System.out.println("How many passengers do you have?");
                 int noOfPassengers = input4.nextInt();
+                // Total number of vehicles needed for the amount of passengers.
                 double totalOfModes = Math.ceil((float) noOfPassengers / (float) transportChoice.getCapacity());
+                // Total cost of all vehicles
                 double totalCost = transportChoice.getCost() * totalOfModes;
-
+                // Plural forms of the user selected vehicle
+                String pluralUserChoice = userchoice;
+                String ticket = "ticket";
+                String rental = "rental";
                 if(totalOfModes > 1 && !userchoice.equalsIgnoreCase("bus")){
-                    userchoice += "s";
+                    pluralUserChoice += "s";
+                    ticket  += "s";
+                    rental  += "s";
+
                 }
+                // Specific to bus object
                 else if (totalOfModes > 1 && userchoice.equalsIgnoreCase("bus")) {
-                    userchoice += "es";
+                    pluralUserChoice += "es";
+                    ticket  += "s";
+                    rental  += "s";
                 }
 
-                System.out.printf("%s%.2f%s%.0f%s", "The total cost is: $",totalCost, " for ", totalOfModes, " " + userchoice);
+                System.out.printf("%s%.2f%s%.0f%s\n", "The total cost is: $",totalCost, " for ", totalOfModes, " " + pluralUserChoice);
+                if (transportChoice.getTicketOrRental().equalsIgnoreCase("Ticket")){
+                    System.out.printf("%s%s%s%s", "Your ", userchoice, " " + ticket, " will be ready at 12:00pm");
+                }else{System.out.printf("%s%s%s%s", "Your ", userchoice, " " + rental, " will be ready at 8:00am");}
             }
+            // While confirmation of transport type is not "yes"
         }while(!confirmation.equalsIgnoreCase("yes"));
     }
 }
